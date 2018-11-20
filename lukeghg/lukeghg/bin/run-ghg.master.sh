e16835df9741875342bfe445e034428844a5aa88
jarip <>
2018-11-20 15:44:14 +0000

    Added comments to describe the script shortly.

--- lukeghg/lukeghg/bin/run-ghg.master.sh
+++ lukeghg/lukeghg/bin/run-ghg.master.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Run 2017 inventory xml import in one step. Note the use of double quotes to allow wild card file reading. 
 source ~/lukeghg/bin/activate
 ghg-master.py -c "/hsan2/khk/ghg/2017/crf/[KPLU]*.csv" -p /hsan2/khk/ghg/2017/FIN_2019_1/PartyProfile-2017-PP.xml\
 	      -x PartyProfileResults_2017.xml\
@@ -6,6 +7,7 @@ ghg-master.py -c "/hsan2/khk/ghg/2017/crf/[KPLU]*.csv" -p /hsan2/khk/ghg/2017/FI
 	      -k /hsan2/khk/ghg/lukeghg/KPLULUSummary/KPSummary.csv\
 	      -l /hsan2/khk/ghg/lukeghg/KPLULUSummary/LUSummary.csv\
 	      -m /hsan2/khk/ghg/lukeghg/300_500_mappings_1.1.csv\
-	      -n "/hsan2/khk/ghg/2017/crf/NIR*.csv" -i "/hsan2/khk/ghg/2017/crf/C*.csv"\
+	      -n "/hsan2/khk/ghg/2017/crf/NIR*.csv"\
+	      -i "/hsan2/khk/ghg/2017/crf/C*.csv"\
 	      -y 2017
 deactivate
