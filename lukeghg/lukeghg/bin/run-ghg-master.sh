7c2d6964643c2f6a866f5476256dc51b28be384c
jarip <jari.perttunen@luke.fi>
2020-09-10 13:40:44 +0300

    Modified command line to 2019 inventory.

--- lukeghg/lukeghg/bin/run-ghg-master.sh
+++ lukeghg/lukeghg/bin/run-ghg-master.sh
@@ -4,13 +4,13 @@
 #See "/hsan2/khk/ghg/lukeghg/InstallAndUpdateLukeGHG.txt" to setup 'lukeghg' python virtual environment. 
 #To redirect output type e.g.: run-ghg-master.sh > output.txt 2> errors.txt.  
 source ~/lukeghg/bin/activate
-ghg-master.py -c "crf/[KPLU]*.csv" -p PartyProfile_2018_template.xml\
-	      -x GHG2018_FIN_2020_4.xml\
+ghg-master.py -c "crf/[KPLU]*.csv" -p PartyProfile/PartyProfile_FIN_2021_1.xml\
+	      -x PartyProfile/PartyProfile_FIN_2021_1_result.xml\
 	      -b lukeghg/KPLULU1990/KP_CM_GM_RV_WDR_UID_notaatioavain.csv\
 	      -k lukeghg/KPLULUSummary/KPSummary.csv\
 	      -l lukeghg/KPLULUSummary/LUSummary.csv\
 	      -m lukeghg/300_500_mappings_1.1.csv\
 	      -n "crf/NIR*.csv"\
 	      -i "crf/C*.csv"\
-	      -y 2018
+	      -y 2019
 deactivate
