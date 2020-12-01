3cee76c37fdc4f8259e26a2a0782e0e7bc9cb0f3
jarip <jari.perttunen@luke.fi>
2020-12-01 16:50:53 +0200

    Changed files for 2019 EU529 inventory

--- lukeghg/lukeghg/bin/run-eu529-ghg-master.sh
+++ lukeghg/lukeghg/bin/run-eu529-ghg-master.sh
@@ -4,12 +4,12 @@
 #See "/hsan2/khk/ghg/lukeghg/InstallAndUpdateLukeGHG.txt" to setup 'lukeghg' python virtual environment. 
 #To redirect output type e.g.: run-eu529-ghg-master.sh > output.txt 2> errors.txt.  
 source ~/lukeghg/bin/activate
-ghg-master.py -c "EU529_2018/KP*.csv" -p GHG2018_EU529_FIN_2020_5_template.xml\
-	      -x GHG2018_EU529_FIN_2020_5.xml\
+ghg-master.py -c "EU529/KP*.csv" -p PartyProfile/PartyProfile_FIN_2021_2.xml\
+	      -x PartyProfile/PartyProfile_FIN_2021_2_result.xml\
 	      -b lukeghg/KPLULU1990/KP_CM_GM_RV_WDR_UID_notaatioavain.csv\
 	      -k lukeghg/KPLULUSummary/KPSummary.csv\
 	      -m lukeghg/300_500_mappings_1.1.csv\
-	      -n "EU529_2018/NIR*.csv"\
-	      -i "EU529_2018/C*.csv"\
-	      -y 2018
+	      -n "EU529/NIR*.csv"\
+	      -i "EU529/C*.csv"\
+	      -y 2019
 deactivate
