9c8b8bfc293d4f1815383f5ffa6d14312810b79a
jarip <jari.perttunen@luke.fi>
2020-12-23 17:24:20 +0200

    Biomass as an example: Calculate Net vchanges and Totals Color summary rows as yellow and missing cells as red.

--- lukeghg/lukeghg/scen/ghgscenario.py
+++ lukeghg/lukeghg/scen/ghgscenario.py
@@ -2,7 +2,8 @@ import glob
 import argparse
 import numpy as np
 import pandas as pd
-import xlsxwriter
+import openpyxl
+import lukeghg.utility.xlmanip as xlp
 import lukeghg.crf.ghginventory as ghginv
 
 def convert_to_float(x:str, keys:bool=True):
@@ -116,13 +117,36 @@ def add_data_series(df_scen,new_row_ls,row_number:int):
     new_row_ls: scenario data series (from the dictionary)
     row_number: the row where the data series belongs to
     """
-    df_scen.loc[row_number,'1990':]=new_row_ls
+    df_scen.loc[row_number,'1990':str(1990+len(new_row_ls)-1)]=new_row_ls
     return df_scen
-              
 
+def write_co2sum_formula(sheet,start_col,ncols,result_row,row_number_ls,color,scale=1):
+    for i in range(start_col,ncols):
+        cell = openpyxl.cell.cell.Cell(sheet,result_row,i)
+        cell_coordinate = cell.coordinate
+        cell_letter = xlp.cut_cell_number(cell_coordinate)
+        cell_ls = []
+        for row_number in row_number_ls:
+            cell_ls.append(cell_letter+str(row_number))
+        for cell_name in cell_ls:
+            if sheet[cell_name].value == "":
+                #It seems ordering of parameters matters in PatternFill!!
+                sheet[cell_name].fill = openpyxl.styles.PatternFill(start_color='00FF0000',fill_type="solid") 
+        formula = '=SUM('
+        for cell_name in cell_ls:
+            formula = formula+cell_name+','
+        #replace last ',' with ')
+        formula = formula[:len(formula)-1]
+        formula = formula+')'+'*'+str(scale)
+        #print(formula)
+        sheet[cell_coordinate]=formula
+        sheet[cell_coordinate].fill = openpyxl.styles.PatternFill(start_color=color, end_color=color,fill_type = "solid")
+    return sheet
 
-def create_scenario_excel(scen_excel_file:str,scen_files_reg_expr:str,uid_excel_file:str,scen_template_file:str,uid_300_500_file:str,keys:bool=False):
-    writer = pd.ExcelWriter(scen_excel_file,engine='xlsxwriter')
+def create_scenario_excel(scen_excel_file:str,scen_files_reg_expr:str,uid_excel_file:str,scen_template_file:str,uid_300_500_file:str,
+                          start_year,end_year,keys:bool=False):
+    missing_uid_dict = dict()
+    writer = pd.ExcelWriter(scen_excel_file,engine='openpyxl')
     d = create_ghg_file_dictionary(scen_files_reg_expr,uid_300_500_file,keys)
     df_uid = read_uid_matrix_file(uid_excel_file)
     df_scen_template = read_scenario_template_file(scen_template_file)
@@ -130,19 +154,45 @@ def create_scenario_excel(scen_excel_file:str,scen_files_reg_expr:str,uid_excel_
     #print("CLASSES",ls)
     for class_name in ls:
         print("LAND USE",class_name)
+        #Initialize missing uid list
+        missing_uid_dict[class_name]=[]
+        #Return for three columns from the UIDMatrix: Number, stock change/emission type, class_name
         df_uid_class=land_use_class_uid_df(df_uid,class_name)
+        #Make a list of uids in class_name columns
         uid_ls = land_use_class_uid_ls(df_uid_class,class_name)
         df_scen_new = df_scen_template.copy()
+        len_current_data_series_ls=0
+        #1.Add time series to dataframe
         for uid in uid_ls:
             (name,id_number) = stock_change_name_id_number(df_uid,class_name,uid)
             #print("UID",uid,"ID",id_number,"NAME",name)
-            data_series_ls = d[uid]
-            #print("DATA",data_series_ls)
-            #print("DIM",np.shape(df_scen_new))
-            row_number = select_row_number(df_scen_template,id_number)
-            #print("ROW",row_number)
-            df_scen_new = add_data_series(df_scen_new,data_series_ls,row_number)
+            if uid in d:
+                data_series_ls = d[uid]
+                len_current_data_series_ls = len(data_series_ls)
+                #print("DATA",data_series_ls)
+                #print("DIM",np.shape(df_scen_new))
+                row_number = select_row_number(df_scen_template,id_number)
+                #print("ROW",row_number)
+                df_scen_new = add_data_series(df_scen_new,data_series_ls,row_number)
+            else:
+                missing_uid_dict[class_name].append(uid)
+        column_ls = df_scen_new.columns
+        year_ls = column_ls[2:]
+        column_ls = ["Number","Source","Unit"]+list(range(start_year,start_year+len(year_ls)-1))
+        df_scen_new.columns = column_ls
         df_scen_new.to_excel(writer,sheet_name=class_name)
+        #2. Add sum rows
+        sheets = writer.sheets
+        sheet = sheets[class_name]
+        #2.1 Biomass Net change
+        write_co2sum_formula(sheet,5,end_year-start_year+1+5,9,[7,8],'00FFFF00',1)
+        #2.2 Biomass Total
+        write_co2sum_formula(sheet,5,end_year-start_year+1+5,14,range(9,14),'00FFFF00',1)
+    missing_uid_df = pd.DataFrame.from_dict(missing_uid_dict,orient='index')
+    sheet_name="UID not in Inventory"
+    missing_uid_df.to_excel(writer,sheet_name=sheet_name)
+    workbook = writer.book
+    workbook.move_sheet(sheet_name,-(len(workbook.sheetnames)-1))
     return writer
 
 if __name__ == "__main__":
@@ -152,8 +202,27 @@ if __name__ == "__main__":
     parser.add_argument('--scen',type=str,dest='scen',help='Scenario excel template file')
     parser.add_argument('--keys',dest='keys',action='store_false',help='Maintain notation keys')
     parser.add_argument('-m',type=str,dest='m',help='UID 300->500 mapping file (CRFReporter)')
-    parser.add_argument('-o',type=str,dest='o',help='Scenario results excel file')
+    parser.add_argument('-o',type=str,dest='o',help='Scenario results excel output file')
+    parser.add_argument('--start',type=int,dest='start',help="Start year (1990)")
+    parser.add_argument('--end',type=int,dest='end',help="End year")
     args = parser.parse_args()
-    excel_writer = create_scenario_excel(args.o,args.files,args.uid,args.scen,args.m,args.keys)
+    if args.files == None:
+        print("No input files")
+    if args.uid == None:
+        print("No UID matrix excel file")
+    if args.scen == None:
+        print("No Scenario excel template file")
+    if args.m == None:
+        print("No UID 300->500 mapping file (CRFReporter)")
+    if args.o == None:
+        print("No Scenario results excel output file")
+    if args.start == None:
+        print("No Start year (1990)")
+    if args.end == None:
+        print("No End year")
+    result = args.files and args.uid and args.scen and args.m and args.o and args.start and args.end
+    if result == None:
+        quit()
+    excel_writer = create_scenario_excel(args.o,args.files,args.uid,args.scen,args.m,args.start,args.end,args.keys)
     excel_writer.save()
     excel_writer.close()
