03109f7455b6eff27f66dd5f7b4b50fc0b2915c3
jarip <jari.perttunen@luke.fi>
2021-02-06 17:58:42 +0200

    checkinventoryvalues.py and convertutf8.py added to scripts section.

--- lukeghg/setup.py
+++ lukeghg/setup.py
@@ -9,5 +9,5 @@ setup(name='lukeghg',
       license='LUKE',
       install_requires=['numpy','pandas','xlsxwriter','xlrd','paramiko','openpyxl'],
       packages=setuptools.find_packages(),
-      scripts=glob.glob('lukeghg/bin/[A-Za-z]*.py')+glob.glob('lukeghg/bin/[A-Za-z]*.sh')
+      scripts=glob.glob('lukeghg/bin/[A-Za-z]*.py')+glob.glob('lukeghg/bin/[A-Za-z]*.sh')+['lukeghg/check/checkinventoryvalues.py','lukeghg/utility/convertutf8.py']
       )
