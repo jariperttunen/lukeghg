f3a7ef1ac9302b7724d4868de207d309123e6b8c
jarip <jari.perttunen@luke.fi>
2021-02-23 10:01:13 +0200

    Updated to FIN_2021_3 inventory

--- lukeghg/lukeghg/bin/run-ghg-master.sh
+++ lukeghg/lukeghg/bin/run-ghg-master.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#run-ghg-master.sh: Run (2017) inventory xml import in one step. 
+#run-ghg-master.sh: Run inventory xml import in one step. 
 #Note the use of double quotes to allow wild card file reading.
-#See "/hsan2/khk/ghg/lukeghg/InstallAndUpdateLukeGHG.txt" to setup 'lukeghg' python virtual environment. 
+#See GitHub https://github.com/jariperttunen/lukeghg InstallAndUpdateLukeGHG.md
+#to setup 'lukeghg' python virtual environment and usage. 
 #To redirect output type e.g.: run-ghg-master.sh > output.txt 2> errors.txt.  
 source ~/lukeghg/bin/activate
-ghg-master.py -c "crf/[KPLU]*.csv" -p PartyProfile/PartyProfile_FIN_2021_1.xml\
-	      -x PartyProfile/PartyProfile_FIN_2021_1_result.xml\
+ghg-master.py -c "crf/[KPLU]*.csv" -p PartyProfile/PartyProfile_FIN_2021_3.xml\
+	      -x PartyProfile/PartyProfile_FIN_2021_3_result.xml\
 	      -b lukeghg/KPLULU1990/KP_CM_GM_RV_WDR_UID_notaatioavain.csv\
 	      -k lukeghg/KPLULUSummary/KPSummary.csv\
 	      -l lukeghg/KPLULUSummary/LUSummary.csv\
