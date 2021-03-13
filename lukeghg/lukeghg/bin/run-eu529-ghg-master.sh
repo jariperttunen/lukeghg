08b1f2b834f5ebd4548390125079e1f1ab207b2f
jarip <jari.perttunen@luke.fi>
2021-03-13 17:02:37 +0200

    Script for 2020 inventory.

--- lukeghg/lukeghg/bin/run-eu529-ghg-master.sh
+++ lukeghg/lukeghg/bin/run-eu529-ghg-master.sh
@@ -4,8 +4,8 @@
 #See "/hsan2/khk/ghg/lukeghg/InstallAndUpdateLukeGHG.txt" to setup 'lukeghg' python virtual environment. 
 #To redirect output type e.g.: run-eu529-ghg-master.sh > output.txt 2> errors.txt.  
 source ~/lukeghg/bin/activate
-ghg-master.py -c "EU529/KP*.csv" -p PartyProfile/PartyProfile_FIN_2021_2.xml\
-	      -x PartyProfile/PartyProfile_FIN_2021_2_result.xml\
+ghg-master.py -c "EU529/KP*.csv" -p PartyProfile/PartyProfile_FIN_2021_4.xml\
+	      -x PartyProfile/PartyProfile_FIN_2021_4_result.xml\
 	      -b lukeghg/KPLULU1990/KP_CM_GM_RV_WDR_UID_notaatioavain.csv\
 	      -k lukeghg/KPLULUSummary/KPSummary.csv\
 	      -m lukeghg/300_500_mappings_1.1.csv\
