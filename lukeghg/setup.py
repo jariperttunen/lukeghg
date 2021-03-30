b5cfc8c1e6623e521795634b2c0c416c923b7d0b
jarip <jari.perttunen@luke.fi>
2021-03-30 18:28:59 +0300

    Added comment regarding pandas 1.2.0+openpyxl problem.
    Added comment regarding pandas 1.2.0+openpyxl problem.
    Fixed pandas to version 1.1.5.

--- lukeghg/setup.py
+++ lukeghg/setup.py
@@ -1,6 +1,10 @@
 import glob
 import setuptools
 from setuptools import setup
+#It seems pandas  1.2.0 and later cannot create  excel files correctly
+#with openpyxl. pandas 1.1.5+openpyxl seems  to be the latest that can
+#generate files excel  can open without warning.  openpyxl version can
+#be the latest.
 setup(name='lukeghg',
       version='1.0',
       description='LUKE GHG Inventory Support Tools',
