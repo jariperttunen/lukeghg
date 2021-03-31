031e1d0e616666b4eb29ad56c1fb5e3b4362cef0
jarip <jari.perttunen@luke.fi>
2021-03-31 11:21:24 +0300

    Added sample dataframe to excel to demonstrate pandas1.2.x+openpyxl cannot create excel file in right format.

--- lukeghg/lukeghg/bin/ghg-scenario.py
+++ lukeghg/lukeghg/bin/ghg-scenario.py
@@ -1,7 +1,8 @@
 #!python
 import argparse
+import pandas as pd
+import openpyxl
 import lukeghg.scen.ghgscenario as ghgscen
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--files',type=str,dest='files',required=True,help='Scenario files (reqular expression)')
@@ -15,8 +16,10 @@ if __name__ == "__main__":
     parser.add_argument('--GWP',type=str,dest='gwp',default="AR5",help="Global warming potential, AR4 (GHG inventory) or AR5 (default)")
     args = parser.parse_args()
     result = args.files and args.uid and args.scen and args.m and args.o and args.start and args.end
+        
     if result == None:
         quit()
+    #Default GWP AR5
     ch4co2eq = 28
     n2oco2eq = 265
     if args.gwp == 'AR4':
@@ -24,6 +27,12 @@ if __name__ == "__main__":
        n2oco2eq = 298
     try:
         excel_writer = ghgscen.create_scenario_excel(args.o,args.files,args.uid,args.scen,args.m,args.start,args.end,args.keys,ch4co2eq,n2oco2eq)
+        #The three lines demonstrating bug(?) in pandas 1.2.0 and above with openpyxl
+        #writing files Excel considers having erroneous format
+        #1 excel_writer = pd.ExcelWriter('Test.xlsx',engine='openpyxl')
+        #2 df = pd.DataFrame([[1,2,3,4,],[5,6,7,8]])
+        #3 df.to_excel(excel_writer,sheet_name='Sheet1')
+        #After save and close erroneous excel file format
         excel_writer.save()
         excel_writer.close()
     except ghgscen.NoInventoryFiles:
