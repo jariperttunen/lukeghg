c7414d5d4ed32869ca18f9d935d44e33a9a236c7
jariperttunen <32894586+jariperttunen@users.noreply.github.com>
2021-09-06 10:19:06 +0300

    Update run-ghg-master.sh
    Try to activate lukeghg in two ways

--- lukeghg/lukeghg/bin/run-ghg-master.sh
+++ lukeghg/lukeghg/bin/run-ghg-master.sh
@@ -4,8 +4,15 @@
 #See GitHub https://github.com/jariperttunen/lukeghg InstallAndUpdateLukeGHG.md
 #to setup 'lukeghg' python virtual environment and usage. 
 #To redirect output type e.g.: run-ghg-master.sh > output.txt 2> errors.txt.  
-#Check python virtual environment for lukeghg is activated:
-source ~/lukeghg/bin/activate
+#Check lukeghg python virtual environment to activate it:
+if [ test -e ~/lukeghg/bin/activate ]
+then
+	source ~/lukeghg/bin/activate
+fi
+elif [ test -e ~/venv/lukeghg/bin/activate ]
+then
+	source  ~/venv/lukeghg/bin/activate
+fi
 ghg-master.py -c "crf/[KPLU]*.csv" -p PartyProfile/PartyProfile_FIN_2021_3.xml\
 	      -x PartyProfile/PartyProfile_FIN_2021_3_result.xml\
 	      -b lukeghg/KPLULU1990/KP_CM_GM_RV_WDR_UID_notaatioavain.csv\
