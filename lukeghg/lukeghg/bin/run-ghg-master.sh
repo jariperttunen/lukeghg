6cf92f6e9f4c10f9f5397e887d2e7b5d5ce3925d
jariperttunen <32894586+jariperttunen@users.noreply.github.com>
2021-09-24 09:05:51 +0300

    Update run-ghg-master.sh

--- lukeghg/lukeghg/bin/run-ghg-master.sh
+++ lukeghg/lukeghg/bin/run-ghg-master.sh
@@ -5,7 +5,8 @@
 #to setup 'lukeghg' python virtual environment and usage. 
 #To redirect output type e.g.: run-ghg-master.sh > output.txt 2> errors.txt.
 #It assumed that this script is in python virtual environment (lukeghg)
-ghg-master.py -c "crf/[KPLU]*.csv" -p PartyProfile/PartyProfile_FIN_2022_2.xml\
+ghg-master.py -c "crf/[KPLU]*.csv"\
+              -p PartyProfile/PartyProfile_FIN_2022_2.xml\
 	      -x PartyProfile/PartyProfile_FIN_2022_2_result.xml\
 	      -b lukeghg/KPLULU1990/KP_CM_GM_RV_WDR_UID_notaatioavain.csv\
 	      -k lukeghg/KPLULUSummary/KPSummary.csv\
