7375ac6cb881e196a26c857e3319ed009effc128
Jari Perttunen <jari.perttunen@luke.fi>
2021-11-06 11:52:53 +0200

    Three options:  1) profuce the table as is, 2) read the table as is and prouce columns to check Total columns and 3) produce summary columns.

--- lukeghg/lukeghg/bin/lulucf-table-641.py
+++ lukeghg/lukeghg/bin/lulucf-table-641.py
@@ -5,35 +5,49 @@ import xlsxwriter
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--input",dest="f1",required=True,help="Read Areas of organic soils (peatlands) of forest land remaining forest land by site type (1 000 ha)")
-    parser.add_argument("-y","--year",dest="f2",required=True,help="GHG inventory year")
+    parser.add_argument("-i","--input",dest="f1",required=True,default="Table_6.4-1_FLRem_Areas_of_organic_soils.csv",
+                        help="Read Areas of organic soils (peatlands) of forest land remaining forest land by site type (1 000 ha)")
+    parser.add_argument("-y","--year",dest="f2",required=True,help="Inventory year")
     parser.add_argument("-o","--output",dest="f3",required=True,help="Output file")
-    parser.add_argument('--format_only',action="store_true",help="Genrate Table 6.4-1 as is") 
+    args_group = parser.add_mutually_exclusive_group(required=False)
+    args_group.add_argument('--format_only',action="store_true",help="Genrate Table 6.4-1 as is")
+    args_group.add_argument('--check_total',action="store_true",help="Calculate self and compare totals")
     args= parser.parse_args()
 
+    df = pd.DataFrame()
     if args.format_only:
         print("Format only")
-        columnls=['Mineral','Undrained','Herb-rich type (Rhtg)','Vaccinium myrtillus type (Mtkg)','Vaccinium vitis-idaea type (Ptkg)',
-                  'Dwarf shrub type (Vatkg)','Caldina type (Jätkg)',"Drained organic","Organic", "Total(Mineral+Organic)"]
-        df = pd.read_table(args.f1,sep=',',names=columnls,skiprows=1)
+        columnls=['Mineral','Undrained','Herb-rich type','Vaccinium myrtillus type','Vaccinium vitis-idaea type',
+                  'Dwarf shrub type','Cladina type',"Drained organic","Total organic","Total"]
+        df = pd.read_table(args.f1,sep=',',usecols=columnls)
+    elif args.check_total:
+        print("Checking total")
+        columnls=['Mineral','Undrained','Herb-rich type','Vaccinium myrtillus type','Vaccinium vitis-idaea type',
+                  'Dwarf shrub type','Cladina type',"Drained organic","Total organic","Total"]
+        df = pd.read_table(args.f1,sep=',',usecols=columnls)
+        df['Drained organic calculated'] = df['Herb-rich type']+df['Vaccinium myrtillus type']+df['Vaccinium vitis-idaea type']+df['Dwarf shrub type']+df['Cladina type']
+        df['Drained organic difference'] = df['Drained organic'] - df['Drained organic calculated']
+        df['Total organic calculated'] = df['Drained organic calculated']+df['Undrained']
+        df['Total organic difference'] = df['Total organic'] - df['Total organic calculated'] 
+        df['Total calculated'] = df['Mineral']+df['Total organic calculated']
+        df["Total difference"] = df['Total'] - df['Total calculated']
     else:
         print("Calculating totals")
-        columnls=['Mineral','Undrained','Herb-rich type (Rhtg)','Vaccinium myrtillus type (Mtkg)','Vaccinium vitis-idaea type (Ptkg)',
-                  'Dwarf shrub type (Vatkg)','Caldina type (Jätkg)']
-        df = pd.read_table(args.f1,sep=',',names=columnls,usecols=range(1,8),skiprows=1)
-        df['Drained organic']=df['Herb-rich type (Rhtg)']+df['Vaccinium myrtillus type (Mtkg)']+df['Vaccinium vitis-idaea type (Ptkg)']+df['Dwarf shrub type (Vatkg)']+df['Caldina type (Jätkg)']
-        df['Organic']=df['Drained organic']+df['Undrained']
-        df['Total (Mineral+Organic)']=df['Mineral']+df['Organic']
+        columnls=['Mineral','Undrained','Herb-rich type','Vaccinium myrtillus type','Vaccinium vitis-idaea type','Dwarf shrub type','Cladina type']
+        df = pd.read_table(args.f1,sep=',',usecols=columnls)
+        df['Drained organic'] = df['Herb-rich type']+df['Vaccinium myrtillus type']+df['Vaccinium vitis-idaea type'] +df['Dwarf shrub type']+df['Cladina type']
+        df['Total organic']=df['Drained organic']+df['Undrained']
+        df['Total (Mineral+Organic)']=df['Mineral']+df['Total organic']
     
     df.index=list(range(1990,int(args.f2)+1))
     df_title=pd.DataFrame(['Table 6.4-1 Areas of organic soils (peatlands) of forest land remaining forest land by site type (1 000 ha)']).transpose()
-    df_explain1=pd.DataFrame(['Drained organic = Herb-rich type (Rhtg)+Vaccinium myrtillus type (Mtkg)+Vaccinium vitis-idaea type (Ptkg)+Drwarf shrub type(Vatkg)+Caldina type (Jätkg)']).transpose()
-    df_explain2=pd.DataFrame(['Organic  = Drained organic+Undrained']).transpose()
-    df_explain3=pd.DataFrame(['Total (Mineral+Organic) = Mineral+Organic']).transpose()
+    df_explain1=pd.DataFrame(['Drained organic = Herb-rich type + Vaccinium myrtillus type + Vaccinium vitis-idaea type + Drwarf shrub type + Cladina type']).transpose()
+    df_explain2=pd.DataFrame(['Total organic  = Drained organic+Undrained']).transpose()
+    df_explain3=pd.DataFrame(['Total = Mineral+ Total organic']).transpose()
     df_datafrom=pd.DataFrame(['Data from: '+args.f1])
     writer=pd.ExcelWriter(args.f3,engine='xlsxwriter')
     df_title.to_excel(writer,'LUTable6-4.1',startrow=0,startcol=0,header=False,index=False)
-    df.to_excel(writer,'LUTable6-4.1',startrow=1,startcol=0)
+    df.to_excel(writer,'LUTable6-4.1',startrow=1,startcol=0,header=True)
     comment_start=len(df.index)+1
     df_explain1.to_excel(writer,'LUTable6-4.1',startrow=comment_start+1,startcol=0,header=False,index=False)
     df_explain2.to_excel(writer,'LUTable6-4.1',startrow=comment_start+2,startcol=0,header=False,index=False)
