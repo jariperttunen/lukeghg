3d7ca6b5dd08a8ae82aa3687ad60e3581963e17f
Jari Perttunen <jari.perttunen@luke.fi>
2021-11-09 09:54:16 +0200

    lulucf-table-641.py mutually exclusive flags --format_only --check_totals mandatory: one of them must be present. Either format existing ipcc land use ito excel file or check totals and produce excel.

--- lukeghg/lukeghg/bin/lulucf-table-641.py
+++ lukeghg/lukeghg/bin/lulucf-table-641.py
@@ -6,12 +6,13 @@ import xlsxwriter
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i","--input",dest="f1",required=True,default="Table_6.4-1_FLRem_Areas_of_organic_soils.csv",
-                        help="Read Areas of organic soils (peatlands) of forest land remaining forest land by site type (1 000 ha)")
+                        help="Read areas of organic soils (peatlands) of forest land remaining forest land by site type (1 000 ha),\n"+
+                              "default Table_6.4-1_FLRem_Areas_of_organic_soils.csv in NIR directory")
     parser.add_argument("-y","--year",dest="f2",required=True,help="Inventory year")
     parser.add_argument("-o","--output",dest="f3",required=True,help="Output file")
-    args_group = parser.add_mutually_exclusive_group(required=False)
-    args_group.add_argument('--format_only',action="store_true",help="Genrate Table 6.4-1 as is")
-    args_group.add_argument('--check_total',action="store_true",help="Calculate self and compare totals")
+    args_group = parser.add_mutually_exclusive_group(required=True)
+    args_group.add_argument('--format_only',action="store_true",help="Generate Table 6.4-1 as is in the input file")
+    args_group.add_argument('--check_total',action="store_true",help="Calculate totals and compare with precalculated totals")
     args= parser.parse_args()
 
     df = pd.DataFrame()
@@ -20,7 +21,7 @@ if __name__ == "__main__":
         columnls=['Mineral','Undrained','Herb-rich type','Vaccinium myrtillus type','Vaccinium vitis-idaea type',
                   'Dwarf shrub type','Cladina type',"Drained organic","Total organic","Total"]
         df = pd.read_table(args.f1,sep=',',usecols=columnls)
-    elif args.check_total:
+    else:
         print("Checking total")
         columnls=['Mineral','Undrained','Herb-rich type','Vaccinium myrtillus type','Vaccinium vitis-idaea type',
                   'Dwarf shrub type','Cladina type',"Drained organic","Total organic","Total"]
@@ -31,14 +32,7 @@ if __name__ == "__main__":
         df['Total organic difference'] = df['Total organic'] - df['Total organic calculated'] 
         df['Total calculated'] = df['Mineral']+df['Total organic calculated']
         df["Total difference"] = df['Total'] - df['Total calculated']
-    else:
-        print("Calculating totals")
-        columnls=['Mineral','Undrained','Herb-rich type','Vaccinium myrtillus type','Vaccinium vitis-idaea type','Dwarf shrub type','Cladina type']
-        df = pd.read_table(args.f1,sep=',',usecols=columnls)
-        df['Drained organic'] = df['Herb-rich type']+df['Vaccinium myrtillus type']+df['Vaccinium vitis-idaea type'] +df['Dwarf shrub type']+df['Cladina type']
-        df['Total organic']=df['Drained organic']+df['Undrained']
-        df['Total (Mineral+Organic)']=df['Mineral']+df['Total organic']
-    
+ 
     df.index=list(range(1990,int(args.f2)+1))
     df_title=pd.DataFrame(['Table 6.4-1 Areas of organic soils (peatlands) of forest land remaining forest land by site type (1 000 ha)']).transpose()
     df_explain1=pd.DataFrame(['Drained organic = Herb-rich type + Vaccinium myrtillus type + Vaccinium vitis-idaea type + Drwarf shrub type + Cladina type']).transpose()
