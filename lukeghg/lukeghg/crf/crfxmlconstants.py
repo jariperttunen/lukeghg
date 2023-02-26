1e9169c6e8d42653e681fb628b14ba1eebec1889
Jari Perttunen <jari.perttunen@luke.fi>
2023-02-26 17:37:04 +0200

    AR5 Global Warming Potenitals in use.

--- lukeghg/lukeghg/crf/crfxmlconstants.py
+++ lukeghg/lukeghg/crf/crfxmlconstants.py
@@ -10,11 +10,11 @@ lulu_hwp_start_year=1961
 #Mineralization 100%, 28.0/44 is the mass of N
 #and the mass on N times 15 gives C.
 n2o_min_c=15.0*100.0*(28.0/44.0)
-#Conversions to CO2eq
+#AR5 Conversions to CO2eq
 #CH4 
-ch4co2eq=25.0
+ch4co2eq=28.0
 #N2O
-n2oco2eq=298.0
+n2oco2eq=265.0
 #C
 ctoco2=44.0/12.0
 nkset={'IE','NA','NO','NE','IO','C','CS','D','NCV','R','T1','T2','T3'}
