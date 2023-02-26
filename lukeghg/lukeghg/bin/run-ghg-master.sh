61c1b36a9105c1e4dbf4e4afd42ab196b73d1789
Jari Perttunen <jari.perttunen@luke.fi>
2023-02-26 17:39:29 +0200

    run-ghg-master.sh for March 2023 submission

--- lukeghg/lukeghg/bin/run-ghg-master.sh
+++ lukeghg/lukeghg/bin/run-ghg-master.sh
@@ -6,13 +6,13 @@
 #To redirect output type e.g.: run-ghg-master.sh > output.txt 2> errors.txt.
 #It assumed that this script is in python virtual environment (lukeghg)
 ghg-master.py -c "crf/[KPLU]*.csv"\
-              -p PartyProfile/PartyProfile_FIN_2022_2.xml\
-	      -x PartyProfile/PartyProfile_FIN_2022_2_result.xml\
+              -p PartyProfile/PartyProfile_FIN_2023_2.xml\
+	      -x PartyProfile/PartyProfile_FIN_2023_2_result.xml\
 	      -b lukeghg/KPLULU1990/KP_CM_GM_RV_WDR_UID_notaatioavain.csv\
 	      -k lukeghg/KPLULUSummary/KPSummary.csv\
 	      -l lukeghg/KPLULUSummary/LUSummary.csv\
 	      -m lukeghg/CRFReporterMappings/300_500_mappings_1.1.csv\
 	      -n "crf/NIR*.csv"\
 	      -i "crf/C*.csv"\
-	      -y 2020
+	      -y 2021
 
